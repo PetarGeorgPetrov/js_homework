deb783a0f44f57648e279508c5039ffcd58b4b49
Petar Petrov <p_petrov92@abv.bg>
2015-12-26 16:03:41 +0200

    js-homework

---
+++ task3.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+
+function myFunction() {
+    alert(document.getElementById("myP").innerHTML);
+}
---
+++ task4.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+function myFunction() {
+	var firstAnchor = document.getElementById('first-anchor').innerHTML;
+	var content = document.getElementById('content').innerHTML = firstAnchor;
+	
+}
